94d2ba43791933e29e347177f531d03019d1248e
James <jamesae8@gmail.com>
2026-09-13 16:37:42 -0500

    Cambio en rama desarrollo

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Mi primer proyecto con Git");
         System.out.println("Segundo cambio realizado");
+        System.out.println("Cambio realizado en desarrollo");
     }
 }

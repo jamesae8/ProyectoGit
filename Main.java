fdaebf42133bcfbece710b05db8bf6fa806292c9
James <jamesae8@gmail.com>
2026-09-13 16:45:33 -0500

    Cambio desde rama conflicto

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Mi primer proyecto con Git");
-        System.out.println("Segundo cambio realizado");
+        System.out.println("Cambio desde conflicto");
         System.out.println("Cambio realizado en desarrollo");
     }
 }

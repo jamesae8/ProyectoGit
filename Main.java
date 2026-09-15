52b80ec411fadf19702f5a1d822dd9caded3ed88
James <jamesae8@gmail.com>
2026-09-14 20:31:02 -0500

    Agrega cambio para demostrar cherry-pick

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Conflicto resuelto correctamente");
         System.out.println("Cambio realizado en desarrollo");
         System.out.println("Cambio realizado desde mi rama");
+        System.out.println("Cambio para demostrar cherry-pick");
     }
 }
